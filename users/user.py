a1f417e1fc6b4a4b0181b2eb3e8d44256b36d541
Saúl Laínez <saullainez@hotmail.es>
2020-06-21 11:35:18 -0600

    Crear clase user
    -Crear constructor.
    -Crear métodos register y login

---
+++ users/user.py
@@ -0,0 +1,12 @@
+class User:
+
+    def __init__(self, name, lastname, email, password):
+        self.name = name
+        self.lastname = lastname
+        self.email = email
+        self.password = password
+    
+    def register(self):
+        
+
+    def login(self):
